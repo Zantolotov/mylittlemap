4c282f5c9d6b3ee8cca3c64e6c02ef3fdf562e78
Zantolotov <antoine.duvard@gmail.com>
2018-04-05 16:27:31 +0200

    Avancement sur aboutMe.html

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import {NavBar} from './component/NavBar/NavBar';
 
 class App extends Component {
   render() {
     return (
+    <div>
+     
       <div className="App">
-          <NavBar />
+          
           <img src={logo} className="App-logo" alt="logo" />
           
         
         <p className="App-intro">
          
         </p>
+        </div>
       </div>
     );
   }
--- src/component/AboutMe/AboutMe.js
+++ src/component/AboutMe/AboutMe.js
@@ -1,23 +1,3 @@
-import React from 'react';
-
-export class AboutMe extends React.Component {
-    render() {
-        return(
-            <div className="container">
-            <div className="row">
-                <div class="col s6">
-                <h2>About Me</h2>
-                
-                
-                </div>
-                <div class="col s3">
-                <h2>Work History</h2>
-                </div>
-                <div class="col s3">
-                <h2>Education</h2>
-                </div>
-            </div>
-            </div>
-        )
-    }
-}
+$(document).ready(function(){
+    $('ul.tabs').tabs();
+});
